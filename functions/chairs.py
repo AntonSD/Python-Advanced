a670a59756e6184d5c93e7a517f1201854bff8ee
AntonSD <anton89.ad@gmail.com>
2022-07-26 10:22:38 +0300

    Create chairs.py

---
+++ functions/chairs.py
@@ -0,0 +1,10 @@
+def generate_combinations(values, index, count, comb):
+    if len(comb) == count:
+        print(comb)
+        return
+
+    for i in range(index, len(values)):
+        comb.append(values[i])
+        generate_combinations(values, i + 1, count, comb)
+        comb.pop()
+
