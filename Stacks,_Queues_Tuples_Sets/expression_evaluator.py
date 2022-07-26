f1ff483b0e2b3637e95a570ebeb435050d7c68a2
AntonSD <anton89.ad@gmail.com>
2022-07-26 10:31:52 +0300

    Create expression_evaluator.py

---
+++ Stacks,_Queues_Tuples_Sets/expression_evaluator.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+expression = input().split()
+nums = deque()
+operations = {
+    '+': lambda a, b: a + b,
+    '-': lambda a, b: a - b,
+    '*': lambda a, b: a * b,
+    '/': lambda a, b: a // b,
+}
+for ch in expression:
+    if ch in "=-*/":
+        while len(nums) > 1:
+            left = nums.popleft()
+            right = nums.popleft()
+            result = operations[ch](left, right)
+            nums.append(result)
+    else:
+        nums.append(int(ch))
+print(nums.popleft())
